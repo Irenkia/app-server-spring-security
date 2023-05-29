2d55dba61dbe5643631fa6273ce9f78ae6b4ebef
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-05-29 10:58:27 +0200

    changed main class and added new dependencies

--- src/main/java/com/example/appserverspringsecurity/AppServerSpringSecurityApplication.java
+++ src/main/java/com/example/appserverspringsecurity/AppServerSpringSecurityApplication.java
@@ -2,11 +2,26 @@ package com.example.appserverspringsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+        AppServerSpringSecurityApplication.class,
+        Jsr310JpaConverters.class
+})
 public class AppServerSpringSecurityApplication {
 
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(AppServerSpringSecurityApplication.class, args);
     }
 
